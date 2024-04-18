4e3d56f16f53255a507d096e4fd69afdbd9fdeaa
abinash kumar <abinash.k@mindstask.com>
2024-04-18 22:57:13 +0530

    allowedhost

--- ams/settings.py
+++ ams/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '18_y&n^=!$k$ojm43-a0)bxsk3&pc_e-=0f##1g#*#)+6=v)g@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
